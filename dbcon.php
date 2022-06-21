f5b8070da1e9918b08a5d4c6ed37dc0342340230
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2022-06-21 23:17:57 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
 // Create connection
 //$con = new mysqli($servername, $username,$password, $db);
-$con = new mysqli("localhost", "root","","fos");
+$con = new mysqli("remotemysql.com:3306", "JFPOT6a49f","pLOz7icXeb","JFPOT6a49f");
 ?>
