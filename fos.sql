9b9fd043c1fbcd02308332a61e79c719571a2836
MadhuryaDutta <iam.madhurya@outlook.com>
2022-06-30 07:18:14 +0530

    Update fos.sql
    Alpha 1.1.1.1.1

--- fos.sql
+++ fos.sql
@@ -1,4 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2022 at 03:46 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fos`
+--
 
 -- --------------------------------------------------------
 
@@ -16,21 +37,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bill`
---
-
-CREATE TABLE `bill` (
-  `bill_id` int(10) NOT NULL,
-  `order_id` int(10) NOT NULL,
-  `item_total` int(5) NOT NULL,
-  `tax` int(5) NOT NULL,
-  `total_amount` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+('alpha', 'alpha');
 
 -- --------------------------------------------------------
 
@@ -49,16 +56,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`cat_id`, `cat_name`, `cat_description`) VALUES
-(1, 'Colddrink', 'assgdfhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
-(2, 'Coldink', 'assgdfhhhhhgggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
-(3, 'Codrink', 'c'),
-(4, 'Colddrik', 'sfdsgdfhfjghkjl;kl'),
-(9, 'Colink', 'nnrtuassgdfhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
-(10, 'Colddeeeeerink', 'assgdfhhhhhhhhhhhhhhhhhgggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
-(11, 'aa', 'ccccc'),
-(100, 'a', 'adasf111111111111111111'),
-(2332, 'Pizza', 'sddssddsd'),
-(5345446, '2343', 'dfddfdfddff');
+(1, 'Soft Drink', 'abcd'),
+(2, 'Juice', 'aaaaaaaaaaa');
 
 -- --------------------------------------------------------
 
@@ -68,7 +67,8 @@ INSERT INTO `category` (`cat_id`, `cat_name`, `cat_description`) VALUES
 
 CREATE TABLE `customer` (
   `cust_id` int(10) NOT NULL,
-  `cust_name` varchar(30) NOT NULL,
+  `cust_fname` varchar(30) NOT NULL,
+  `cust_lname` varchar(30) NOT NULL,
   `cust_password` varchar(30) NOT NULL,
   `phone` int(10) NOT NULL,
   `email` varchar(30) NOT NULL,
@@ -79,8 +79,8 @@ CREATE TABLE `customer` (
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`cust_id`, `cust_name`, `cust_password`, `phone`, `email`, `address`) VALUES
-(1, 'Madhurya Dutta', 'alpha', 1122, 'dmadhurya@protonmail.com', 'Lakhimpur , Assam');
+INSERT INTO `customer` (`cust_id`, `cust_fname`, `cust_lname`, `cust_password`, `phone`, `email`, `address`) VALUES
+(1, 'Madhurya', 'Dutta', 'alphabeta', 2147483647, 'Iam@dutta.com', 'Lakhimpur, Assam');
 
 -- --------------------------------------------------------
 
@@ -93,39 +93,73 @@ CREATE TABLE `items` (
   `item_name` varchar(25) NOT NULL,
   `item_price` int(5) NOT NULL,
   `item_desc` varchar(100) NOT NULL,
-  `cat_id` int(5) NOT NULL
+  `cat_id` int(5) NOT NULL,
+  `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_id`, `item_name`, `item_price`, `item_desc`, `cat_id`) VALUES
-(1, 'pizza', 100, 'faaaaaaaaaaaaaawberyhtuynm', 2),
-(11, 'piuytkt45zza', 1600, 'fawberyhvteretuynm', 4),
-(12, 'pi446zza', 1200, 'aaaaaaaaaawvreveberyhtuynm', 2),
-(1267, 'pi446zza', 1200, 'aaaaaaaaaawvreveberyhtuynm', 2);
-
--- --------------------------------------------------------
+INSERT INTO `items` (`item_id`, `item_name`, `item_price`, `item_desc`, `cat_id`, `image`) VALUES
+(42, 'Allu Pitika', 100, 'Allu & Pitika', 2, 'card-8-1656538640.jpg');
 
 --
--- Table structure for table `order_items`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE `order_items` (
-  `s_no` int(10) NOT NULL,
-  `order_id` int(50) NOT NULL,
-  `cust_id` int(10) NOT NULL,
-  `item_id` int(10) NOT NULL,
-  `order_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
 
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cust_id`);
 
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `cat_id` (`cat_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
-CREATE TABLE `admin` (  `username` varchar(30)  NOT NULL,  `password` varchar(30) NOT NULL)
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
-CREATE TABLE `customer` ( `cust_id` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, `cust_fname` varchar(30) NOT NULL,`cust_lname` varchar(30) NOT NULL, `cust_password` varchar(30) NOT NULL, `phone` int(10) NOT NULL, `email` varchar(30) NOT NULL, `address` varchar(100) NOT NULL );
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
-CREATE TABLE `category` ( `cat_id` int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT, `cat_name` varchar(25) NOT NULL, `cat_description` varchar(100) NOT NULL );
-CREATE TABLE `items` ( `item_id` int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT, `item_name` varchar(25) NOT NULL, `item_price` int(5) NOT NULL, `item_desc` varchar(100) NOT NULL, `cat_id` int(5) NOT NULL, FOREIGN KEY (cat_id) REFERENCES category(cat_id) );
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `item_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
